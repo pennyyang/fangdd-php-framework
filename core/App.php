fd44f910175f7b2d1b13c45751e677f3f94fec7e
picasso250 <cumt.xiaochi@gmail.com>
2013-04-23 08:01:06 +0800

    app structure

--- core/App.php
+++ core/App.php
@@ -5,9 +5,21 @@
 */
 class App
 {
+    private static $app;
     
-    function __construct(argument)
+    /**
+     * 私有的构造函数，在单例模式中防止误初始化
+     */
+    private function __construct()
     {
         # code...
+    }
+
+    /**
+     * 通过这个方法获得单例
+     */
+    public static function app()
+    {
+        return $app;
     }
 }
